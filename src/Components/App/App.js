d1b02135241bda07af0168e3022dfbca4a01fb7e
Victor <nastoyashchiy@gmail.com>
2023-10-07 02:12:44 +0300

    1

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,15 +5,22 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
+import Spotify from '../../util/Spotify';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id:1}, {name: 'name2', artist: 'artist2', album: 'album2', id:2}, {name: 'name3', artist: 'artist3', album: 'album3', id:3}],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'plylistAlbum1', id:4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'plylistAlbum2', id:5},{name: 'playlistName3', artist: 'playlistArtist3', album: 'plylistAlbum3', id:6}]
-    }
+      playlistTracks: []
+    };
+    this.addTract = this.addTract.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTract(track) {
@@ -23,7 +30,35 @@ class App extends React.Component {
     }
 
     tracks.push(track);
-    this.setState({playlistTracks: tracks});
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
+  }
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({ searchResults: searchResults })
+    })   
   }
 
   render() {
@@ -34,11 +69,15 @@ class App extends React.Component {
           <span className="writtenName"> The Playlist</span>
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} 
+                           onAdd={this.addTract}/>
             <Playlist playlistName={this.state.playlistName}
-                      playlistTracks={this.state.playlistTracks} />
+                      playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
           </div>
         </div>      
       </div>
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,36 @@ import React from "react";
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            term: ''
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+          this.search();
+        }
+    }
+    
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input onChange={this.handleTermChange} onKeyPress={this.handleKeyPress} placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
+                
             </div>
         )
     }
